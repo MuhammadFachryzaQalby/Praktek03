c0109fe942282bc95a6aa9d25f75559c091141f0
qalbyreza@gmail.com <qalbyreza@gmail.com>
2023-05-26 10:39:09 +0800

    instance,Buat atribut

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -2,6 +2,12 @@ package praktek03;
 
 public class SepedaAksi {
     public static void main(String[] args) {
-        
+        Sepeda s = new Sepeda();
+        s.merk="Kupu-Kupu";
+        s.warna="Kuning";
+        s.harga=2000;
+
+        s.cetakInfo();
+
     }
 }
