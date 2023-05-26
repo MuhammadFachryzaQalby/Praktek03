51059fe05614311de8d877f1e02791508b1674ba
qalbyreza@gmail.com <qalbyreza@gmail.com>
2023-05-26 10:48:21 +0800

    Menggunakan  Method Set

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -3,9 +3,9 @@ package praktek03;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
-        s.merk="Kupu-Kupu";
-        s.warna="Kuning";
-        s.harga=2000;
+        s.setMerk("Kupu-Kupu");
+        s.setWarna("Kuning");
+        s.setHarga(2000);
 
         s.cetakInfo();
 
